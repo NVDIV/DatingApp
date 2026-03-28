35026db777683e36f5a2414b2a3b74b77bf7d918
Nadiia Rybak <nvdiv5@gmail.com>
2026-03-28 18:22:23 +0200

    feat(services): core services and methods

---
+++ src/main/java/com/dating/app/services/FeedService.java
@@ -0,0 +1,16 @@
+package com.dating.app.services;
+
+import com.dating.app.dto.UserCardDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FeedService {
+    List<UserCardDTO> descovery() {
+        // TODO
+        return null;
+    }
+}
---
+++ src/main/java/com/dating/app/services/MatchService.java
@@ -0,0 +1,14 @@
+package com.dating.app.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class MatchService {
+    void createMatch(UUID user1Id, UUID user2Id) {
+        // TODO
+    }
+}
